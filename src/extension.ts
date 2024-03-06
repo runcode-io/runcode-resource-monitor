810488f5f9e96b9f2029401312e26bfb9b9144cb
Pranith12250 <127399614+Pranith12250@users.noreply.github.com>
2024-03-06 13:46:34 +0530

    Initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,20 @@
 import * as vscode from 'vscode';
 import prettyBytes from 'pretty-bytes';
-import { currentLoad, fsStats, mem, networkStats } from "systeminformation"
+import { currentLoad, fsSize, mem } from "systeminformation"
 
 let statusBarMemoryItem: vscode.StatusBarItem;
+let statusCpuItem: vscode.StatusBarItem;
+let statusDiskItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
 
 	statusBarMemoryItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right
+	);
+	statusCpuItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right
+	);
+	statusDiskItem = vscode.window.createStatusBarItem(
 		vscode.StatusBarAlignment.Right
 	);
 	setInterval(memText, 3000);
@@ -14,8 +22,24 @@ export function activate(context: vscode.ExtensionContext) {
 
 const memText = async () => {
 	const m = await mem();
-	statusBarMemoryItem.text= `Memory: ${prettyBytes(m.active)}`;
+	const cl = await currentLoad();
+	let fsSizes = await fsSize();
+	let totalDiskSize=0;
+	let totalDiskUsed=0;
+	for (let fsSize of fsSizes) {
+		totalDiskSize+=fsSize.size;
+		totalDiskUsed+=fsSize.used;
+	}
+
+	const totalDisk = fsSizes[0].size / 1024 / 1024 / 1024;
+	const usedDisk = fsSizes[0].used / 1024 / 1024 / 1024;
+
+	statusBarMemoryItem.text= `$(server) ${prettyBytes(m.active)}`;
 	statusBarMemoryItem.show();
+	statusCpuItem.text= `$(pulse)${cl.currentLoad.toFixed(2)}%`;
+	statusCpuItem.show();
+	statusDiskItem.text= `$(database)${prettyBytes(totalDiskUsed)}/${prettyBytes(totalDiskSize)} | ${prettyBytes(usedDisk)}/${prettyBytes(totalDisk)}`;
+	statusDiskItem.show();
   };
 
 // This method is called when your extension is deactivated
